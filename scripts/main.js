f89aef60217f57d798f952d5cbb71bdd9e81b76b
ndemasukwo <nde23002@byui.edu>
2024-02-27 17:59:32 +0100

    Week 1

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+// An example of declaring a variable in JavaScript by calling them in separate lines.
+// let myVariable;
+// myVariable = "Bob";
+
+// Another example of declaring a variable, but in the same line.
+let myVariable = "Bob";
+myVariable = "Steve";
+/* To signify that the value is a string enclose it in single or double quotes.
+Of numbers there are no quotations.
+Same as numbers for the Boolean True/False value
+Now with Array example "let myVariable = [1, 'Bob','Steve',10];
+/*
